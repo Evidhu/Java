9a81e2414601ad49a423cbc03316b2a393e78e30
Evidhu <vidhu145cs@gmail.com>
2014-10-30 08:26:29 +0530

    question partially done

---
+++ udp/MyClass.java
@@ -0,0 +1,17 @@
+import java.net.InetAddress;
+public class MyClass {
+    int m,n,p,q;
+    int[][] a,b;
+    InetAddress ip;
+	
+    public MyClass(int m,int n,int p,int q,int[][]a,int[][]b,InetAddress ip) {
+        this.a=a;
+        this.b=b;
+        this.m=m;
+        this.n=n;
+        this.p=p;
+        this.q=q;
+        this.ip=ip;
+    }
+    
+}
